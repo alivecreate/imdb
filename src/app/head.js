e01a478e812d810afb732e25f9f80cf8abfa585f
Krishna Kahar <myalivecreate@gmail.com>
2023-08-12 19:00:41 +0530

    Clear Some Old data

--- src/app/head.js
+++ src/app/head.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Head() {
   return (
+<>
     <title>IMDB App</title>
+    <link rel="icon" type="image/x-icon" href="../../img/favicon.png" />
+    </>
   )
 }
 
